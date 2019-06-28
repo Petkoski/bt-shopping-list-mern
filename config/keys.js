69a623ca96b0e8c9df3688aa121bfbaf582a180b
Petkoski <petkoski15@hotmail.com>
2019-06-28 21:07:16 +0200

    Included keys.js

---
+++ config/keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://jovan:6X15TZ2khZva4lN3@cluster0-dcbbk.mongodb.net/bt-mern-shopping-list?retryWrites=true&w=majority',
+    dbName: 'bt-mern-shopping-list'
+}
+
+/**
+ * mongodb+srv://jovan:<password>@cluster0-dcbbk.mongodb.net/test?retryWrites=true&w=majority
+ * jovan
+ * 6X15TZ2khZva4lN3
+ */
